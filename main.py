40bc0fc007b4b85cff776164567bc88061b8cf15
Yuqing Lu <luyuqing0708@gmail.com>
2020-03-02 02:58:16 +0100

    started using google sheets

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import os
 
 from flask import Flask, render_template
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from apiclient.discovery import build
 
 
 KEY = "AIzaSyCU8AOyOFfPBH5aZnphqXhg6aeD6dix0nk"
+SHEET_ID = "1BuG9Hn0S7r-8KmtKMawUCq2tonhNEbWKIJ4Y-u8IhGo"
 
 app = Flask(__name__)
+
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive'
+    ]
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("yuqing@coronasmittetinorge.iam.gserviceaccount.com").sheet1
+data = sheet.get_all_records()
+labels = []
+locations = []
+for row in data:
+    labels.append(row['label'])
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", key=KEY)
+    return render_template("index.html", labels=labels)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
