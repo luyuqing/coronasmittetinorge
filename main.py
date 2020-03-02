d63b34627fdb90da25ceb2fc7bfd26322e33ed5d
Yuqing <yuqing@boost.ai>
2020-03-02 15:21:43 +0100

    google search verify

--- main.py
+++ main.py
@@ -12,5 +12,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", key=KEY)
 
+
+@app.route('/google3c8d8110329b8523.html')
+def google_search_verify():
+    return render_template("google3c8d8110329b8523.html")
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
