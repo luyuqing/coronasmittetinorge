49b8b77e9a23d23def8e758c2b8dcd521b279b4a
Yuqing Lu <luyuqing0708@gmail.com>
2020-03-02 22:38:20 +0100

    updated 202003021030

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
 import os
 
-from flask import Flask, render_template
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from apiclient.discovery import build
+from flask import (
+    Flask,
+    render_template,
+    request,
+    jsonify,
+)
 
-
-KEY = "AIzaSyCU8AOyOFfPBH5aZnphqXhg6aeD6dix0nk"
-SHEET_ID = "1BuG9Hn0S7r-8KmtKMawUCq2tonhNEbWKIJ4Y-u8IhGo"
 
 app = Flask(__name__)
-
-scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive.file',
-    'https://www.googleapis.com/auth/drive'
-    ]
-creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-client = gspread.authorize(creds)
-sheet = client.open("yuqing@coronasmittetinorge.iam.gserviceaccount.com").sheet1
-data = sheet.get_all_records()
-labels = []
-locations = []
-for row in data:
-    labels.append(row['label'])
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", labels=labels)
+    return render_template("index.html")
 
 
 @app.route('/google3c8d8110329b8523.html')
 def google_search_verify():
     return render_template("google3c8d8110329b8523.html")
+
+
+@app.route('/history')
+def history():
+    dt = request.args['dt']
+    template = dt + '.html'
+    return render_template(template)
 
 
 if __name__ == '__main__':
